b688962388cb32faa83557712eb21cad124931dc
Ramesh Nair <ram@hiddentao.com>
2016-04-04 13:31:44 +0800

    fix test

--- test/core.test.js
+++ test/core.test.js
@@ -113,7 +113,10 @@ test['geth options'] = {
       .then(() => {
         var web3 = new Web3();
         web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'));
-        web3.eth.coinbase.should.eql('');
+
+        expect(() => {
+          return web3.eth.coinbase;
+        }).to.throw('Invalid JSON RPC response: undefined');
       })
       .asCallback(done);
   }
